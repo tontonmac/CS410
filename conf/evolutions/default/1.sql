7ededf36e2ac11db22dd22a18c168cce4ed22006
Joe Dollard <jdollard@gmail.com>
2015-03-01 13:53:17 -0500

    schema improvements: * rename tables to conform with play standards * create data insertion script for testing * create schema diagram

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,47 +2,51 @@
 -- how database migrations (aka evolutions) work in the play framework
 
 -- To generate a schema diagram for this:
--- java -jar schemaSpy_5.0.0.jar -t mysql -db bookstore -host localhost -u root -dp mysql-connector-java-5.1.34-bin.jar -o ~/db_output/
+-- java -jar schemaSpy_5.0.0.jar -t mysql -db textbook_finder -host localhost -u root -dp mysql-connector-java-5.1.34-bin.jar -o ~/db_output/
 
 # --- !Ups
 
-CREATE TABLE departments (
+CREATE TABLE department (
     id int NOT NULL AUTO_INCREMENT,
     full_name varchar(128) NOT NULL,
     short_name varchar(16) NOT NULL,
+    UNIQUE (full_name),
+    UNIQUE (short_name),
     PRIMARY KEY (id)
 );
 
-CREATE TABLE terms (
+CREATE TABLE term (
     id int NOT NULL AUTO_INCREMENT,
     name varchar(32) NOT NULL,
     PRIMARY KEY (id),
     UNIQUE (name)
 );
 
-CREATE TABLE courses (
+CREATE TABLE course (
     id int NOT NULL AUTO_INCREMENT,
     department_id int,
     name varchar(255) NOT NULL,
+    number varchar(8) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES departments(id),
+    FOREIGN KEY (department_id) REFERENCES department(id),
+    UNIQUE (department_id, number),
     INDEX (department_id)
 );
 
-CREATE TABLE classes (
+CREATE TABLE class (
     id int NOT NULL AUTO_INCREMENT,
     term_id int NOT NULL,
     course_id int NOT NULL,
     section_number varchar(8) NOT NULL,
     instructor_name varchar(128),
     PRIMARY KEY (id),
-    FOREIGN KEY (course_id) REFERENCES courses(id),
-    FOREIGN KEY (term_id) REFERENCES terms(id),
+    FOREIGN KEY (course_id) REFERENCES course(id),
+    FOREIGN KEY (term_id) REFERENCES term(id),
     INDEX (course_id),
     UNIQUE(term_id,course_id,section_number)
 );
 
-CREATE TABLE users (
+CREATE TABLE user (
     id int NOT NULL AUTO_INCREMENT,
     email varchar(255) NOT NULL,
     password varchar(64) NOT NULL,
@@ -51,45 +55,45 @@ CREATE TABLE users (
     department_id int,
     photo_url varchar(1024),
     PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES departments(id),
+    FOREIGN KEY (department_id) REFERENCES department(id),
     UNIQUE(email)
 );
 
-CREATE TABLE conditions (
+CREATE TABLE book_condition (
     id int NOT NULL AUTO_INCREMENT,
     name varchar(16) NOT NULL,
     PRIMARY KEY (id),
     UNIQUE(name)
 );
 
-CREATE TABLE book_listings (
+CREATE TABLE book_listing (
     id int NOT NULL AUTO_INCREMENT,
     listed_by_id int,
     department_id int,
     description varchar(5120),
     price decimal(8,2),
-    title varchar(512),
+    title varchar(1024),
     author varchar(512),
-    isbn bigint,
+    isbn varchar(24),
     copyright_date date,
     publisher varchar(512),
     image_path varchar(256),
     edition varchar(16),
     num_views int,
-    condition_id int,
+    book_condition_id int,
     PRIMARY KEY (id),
-    FOREIGN KEY (listed_by_id) REFERENCES users(id),
-    FOREIGN KEY (department_id) REFERENCES departments(id),
-    FOREIGN KEY (condition_id) REFERENCES conditions(id),
+    FOREIGN KEY (listed_by_id) REFERENCES user(id),
+    FOREIGN KEY (department_id) REFERENCES department(id),
+    FOREIGN KEY (book_condition_id) REFERENCES book_condition(id),
     INDEX (listed_by_id),
     INDEX (isbn)
 );
 
-CREATE TABLE books (
+CREATE TABLE book (
     id int NOT NULL AUTO_INCREMENT,
-    title varchar(512),
+    title varchar(512) NOT NULL,
     author varchar(512),
-    isbn bigint,
+    isbn varchar(24),
     copyright_date date,
     publisher varchar(512),
     image_path varchar(256),
@@ -98,39 +102,40 @@ CREATE TABLE books (
     UNIQUE (isbn)
 );
  
-CREATE TABLE required_books (
+CREATE TABLE required_book (
     id int NOT NULL AUTO_INCREMENT,
     class_id int NOT NULL,
     book_id int NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (book_id) REFERENCES books(id),
-    FOREIGN KEY (class_id) REFERENCES classes(id),
+    FOREIGN KEY (book_id) REFERENCES book(id),
+    FOREIGN KEY (class_id) REFERENCES class(id),
     UNIQUE (class_id,book_id)
 );
 
-CREATE TABLE book_prices (
+CREATE TABLE book_price (
     id int NOT NULL AUTO_INCREMENT,
     book_id int NOT NULL,
     buy_new_price decimal(8,2),
     buy_used_price decimal(8,2),
-    rent_price decimal(8,2),
+    rent_new_price decimal(8,2),
+    rent_used_price decimal(8,2),
     url varchar(1024),
     PRIMARY KEY (id),
-    FOREIGN KEY (book_id) REFERENCES books(id),
+    FOREIGN KEY (book_id) REFERENCES book(id),
     UNIQUE (book_id)
 );
 
 
 # --- !Downs
  
-DROP TABLE book_prices;
-DROP TABLE required_books;
-DROP TABLE books;
-DROP TABLE book_listings;
-DROP TABLE conditions;
-DROP TABLE users;
-DROP TABLE classes;
-DROP TABLE courses;
-DROP TABLE terms;
-DROP TABLE departments;
+DROP TABLE book_price;
+DROP TABLE required_book;
+DROP TABLE book;
+DROP TABLE book_listing;
+DROP TABLE book_condition;
+DROP TABLE user;
+DROP TABLE class;
+DROP TABLE course;
+DROP TABLE term;
+DROP TABLE department;
 
---
+++ other/test_data.sql
@@ -0,0 +1,39 @@
+use textbook_finder;
+
+insert into department(full_name,short_name) values ("Computer Science", "CS");
+insert into department(full_name,short_name) values ("Chemistry", "CHEM");
+
+insert into course(id, department_id,name, number) values (1, 1, "Introduction to Computing", "101");
+insert into course(id, department_id,name, number) values (2, 1, "Programming in C", "240");
+insert into course(id, department_id,name, number) values (3, 1, "Applied Discrete Mathematics", "320L");
+insert into course(id, department_id,name, number) values (4, 2, "Physiological Chemistry", "130");
+
+insert into term(id, name) values (1, "Spring 2015");
+insert into term(id, name) values (2, "Summer 2015");
+
+insert into class(id,term_id,course_id,section_number,instructor_name) values (1, 1, 4, "01", "Dransfield,Timothy");
+insert into class(id,term_id,course_id,section_number,instructor_name) values (2, 1, 4, "01D", "Dransfield,Timothy");
+insert into class(id,term_id,course_id,section_number,instructor_name) values (3, 1, 1, "05", "Ghorbanzade,Pejman");
+insert into class(id,term_id,course_id,section_number,instructor_name) values (4, 1, 2, "01", "Tran,Duc");
+insert into class(id,term_id,course_id,section_number,instructor_name) values (5, 1, 2, "02", "Cheung,Ronald S");
+
+insert into book(id,title,author,isbn,publisher) values
+    (1, "C Programming Language","Kernighan", "007-6092003106", "Prentice Hall");
+insert into book(id,title,author,isbn,publisher) values
+    (2, "Intro to Programming in Java","Sedgewick", "978-0321498052", "Addison-Wesley");
+
+insert into book(id,title,author,isbn,publisher) values
+    (3, "MasteringChemistry with Pearson eText -- Standalone Access Card -- for Chemistry: A Molecular Approach (3rd Edition)",
+     "Nivaldo J. Tro",
+     "978-0321806383",
+     "Prentice Hall");
+
+insert into required_book(class_id,book_id) values (1, 3);
+insert into required_book(class_id,book_id) values (2, 3);
+insert into required_book(class_id,book_id) values (3, 2);
+insert into required_book(class_id,book_id) values (4, 1);
+insert into required_book(class_id,book_id) values (5, 1);
+
+insert into book_price(book_id, buy_new_price, buy_used_price, rent_new_price, rent_used_price) values
+    (1, 67.00, 50.25, 46.90, 33.50);
+
