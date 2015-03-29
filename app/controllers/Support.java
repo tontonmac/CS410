695a974a64950dc3694e87bdaa3b4881d3a1525a
David Lowery <lowery@cs.umb.edu>
2015-03-29 17:12:51 -0400

    Adding support page

---
+++ app/controllers/Support.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import views.html.*;
+
+public class Support extends Controller {
+
+    public static Result support() {
+        return ok(support.render("Your new application is ready."));
+    }
+
+}
