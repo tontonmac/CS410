caa0f3ac1ed07d30b48b0b38e562e2e110adf8a4
David Lowery <lowery@cs.umb.edu>
2015-03-29 16:49:17 -0400

    Adding rough draft for buying pages.

---
+++ app/controllers/Buying.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import views.html.*;
+
+public class Buying extends Controller {
+
+    public static Result buy() {
+        return ok(buy.render("Your new application is ready."));
+    }
+
+}
