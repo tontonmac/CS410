72b48ff4e972064d7bd5368f86250a6c11eb8f28
Joe Dollard <jdollard@gmail.com>
2015-03-29 17:41:43 -0400

    placeholders for book actions

---
+++ app/controllers/Book.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import views.html.*;
+
+public class Book extends Controller {
+
+    public static Result newBook() {
+        return ok(newBook.render());
+    }
+
+    public static Result editBook() {
+        return ok(editBook.render());
+    }
+
+    public static Result showBook() {
+        return ok(showBook.render());
+    }
+
+}
