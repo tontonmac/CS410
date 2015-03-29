8c9f57c5398606e5e91e6098027b094d7aabb1c0
David Lowery <lowery@cs.umb.edu>
2015-03-29 17:53:05 -0400

    Adding delete and linking listings to methods in Book controller

--- app/controllers/Book.java
+++ app/controllers/Book.java
@@ -17,5 +17,9 @@ public class Book extends Controller {
     public static Result showBook() {
         return ok(showBook.render());
     }
+    
+    public static Result deleteBook() {
+    	 return ok(listings.render());
+    }
 
 }
