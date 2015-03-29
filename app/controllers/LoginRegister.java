973126b8755d7648019fb3299f9b13160fbaa704
David Lowery <lowery@cs.umb.edu>
2015-03-29 16:12:13 -0400

    Adding rough draft of login page and LoginRegister class for business logic.

---
+++ app/controllers/LoginRegister.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import views.html.*;
+
+public class LoginRegister extends Controller {
+
+    public static Result login() {
+        return ok(login.render("Your new application is ready."));
+    }
+
+}
