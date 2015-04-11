df3f11fad9349e42cd1df055f9e4238b50b0b13c
David Lowery <lowery@cs.umb.edu>
2015-04-11 11:54:43 -0400

    Implementation of the login page. Only validates a user and attaches his or her info to the session. Still need to add authentication to the rest of the pages.

--- app/controllers/LoginRegister.java
+++ app/controllers/LoginRegister.java
@@ -1,13 +1,43 @@
 package controllers;
 
+import models.User;
 import play.*;
+import play.data.*;
 import play.mvc.*;
 import views.html.*;
 
 public class LoginRegister extends Controller {
 
-    public static Result login() {
-        return ok(login.render());
-    }
+	public static class Login {
 
+	    public String email;
+	    public String password;
+
+	    public String validate() {
+	        if (User.authenticate(email, password) == null) {
+	          return "Invalid user or password";
+	        }
+	        return null;
+	    }
+	}
+
+    public static Result login() {
+    	Form<Login> form = Form.form(Login.class);
+        return ok(
+            login.render(form)
+        );
+    }
+    
+    public static Result authenticate() {
+    	Form<Login> form = Form.form(Login.class).bindFromRequest();
+        if (form.hasErrors()) {
+            return badRequest(login.render(form));
+        } else {
+            session().clear();
+            session("email", form.get().email);
+            return redirect(
+                routes.Application.index()
+            );
+        }
+    }
 }
--- app/models/User.java
+++ app/models/User.java
@@ -1,14 +1,17 @@
 package models;
 
 import java.net.URL;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
 import play.data.validation.Constraints;
 import play.db.ebean.Model;
+import play.db.ebean.Model.Finder;
 
 @Entity
 public class User extends Model {
@@ -25,9 +28,24 @@ public class User extends Model {
 	public String first_name;
 	public String last_name;
 	
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "department_id", referencedColumnName = "id")
     public Department department;
     
     public String photo_url;
+    
+    public static User authenticate(String email, String password) {
+    	User user = findUserByEmail(email);
+    	if (user.password.equals(password)) {
+    		return user;
+    	}
+    	
+    	return null;
+    }
+    
+    public static Finder<Long,User> find = new Finder<Long,User>(Long.class, User.class);
+    
+    public static User findUserByEmail(String email) {
+        return find.where().eq("email", email).findUnique();
+    }
 }
