9d862e08dd2b58fdccebbe6e24ec6b3de45c68fb
Mohamed <Mohamed@10.0.0.3>
2015-04-12 01:03:51 -0400

    this is what i have so far. NOT WORKING :(

--- app/controllers/Book.java
+++ app/controllers/Book.java
@@ -1,7 +1,16 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import models.Listing;
 import play.*;
 import play.mvc.*;
+import views.html.*;
+import play.data.DynamicForm;
+import play.data.Form;
+import java.util.Collections;
+import play.mvc.Http.Request;
+import scala.collection.JavaConversions;
 import views.html.*;
 
 public class Book extends Controller {
@@ -18,11 +27,28 @@ public class Book extends Controller {
 
 	@Security.Authenticated(Secured.class)
     public static Result showBook(Long bookId) {
-        return ok(showBook.render());
+		
+//		
+//		DynamicForm requestData = Form.form().bindFromRequest();
+//		
+//		String id = requestData.get("book_listing");
+//		
+//		List<Listing> sellerlisting = Listing.findListingsBySeller(id);
+//		
+//		List<Listing> list = Book.find(Listing.class)
+//					.join("user")
+//					.where()
+//					.gt("id", 0)
+//					.eq("book_listing", models.Listing.id)
+//					.findList();
+//			System.out.println(list);
+		return ok(showBook(Long).render());
     }
     
 	@Security.Authenticated(Secured.class)
     public static Result deleteBook() {
+		Listing.createUpdate(Book.class, "delete from book_listing")
+		.execute();
     	 return ok(listings.render());
     }
 
