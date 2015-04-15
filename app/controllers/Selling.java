e49e7357342a74b62cfb9052d96acfc02cd3e453
aadi888 <adish888@gmail.com>
2015-04-14 21:00:24 -0400

    listings
    some problem with CSS when 2 or more recoreds are there

--- app/controllers/Selling.java
+++ app/controllers/Selling.java
@@ -1,14 +1,24 @@
 package controllers;
 
+import java.util.List;
+
 import play.*;
 import play.mvc.*;
+import play.mvc.Http.Request;
 import views.html.*;
+import models.Course;
+import models.Listing;
+import models.UMBClass;
 
 public class Selling extends Controller {
 
-	@Security.Authenticated(Secured.class)
-    public static Result listings() {
-        return ok(listings.render());
-    }
 
+
+
+    @Security.Authenticated(Secured.class)
+	public static Result listings() {
+    	String listedBy=session().get("userid");
+    	List<Listing> listings12 = Listing.findListingsBySeller(listedBy);
+        return ok(listings.render(listings12));
+    }
 }
